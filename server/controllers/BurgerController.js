b930dab5eafec570b6aaf0f8a2d68d3880a0a3e0
jasonthealmighty@gmail.com <jasonthealmighty@gmail.com>
2021-01-11 15:31:34 -0700

    controller and service

---
+++ server/controllers/BurgerController.js
@@ -0,0 +1,18 @@
+import BaseController from "../utils/BaseController";
+
+export class BurgerController extends BaseController {
+    constructor(){
+        super("api/burgers");
+        this.router
+            .get("", this.getAll)
+            .post("", this.create);
+    }
+    async getAll(req, res, next) {
+        try{
+            return res.send(["burger1, burger2"]);
+        }
+        catch (error) {
+            next(error);
+        }
+    }
+}
---
+++ server/services/BurgerService.js
@@ -0,0 +1,10 @@
+import { dbContext } from "../db/DbContext";
+
+class BurgerService{
+    async find(query={}){
+        let burgers = await dbContext.Burgers.find(query);
+        return burgers;
+    }
+}
+
+export const burgerService = new BurgerService();
